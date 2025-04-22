f4c58d216cb8265c1ce6b402782463f17d0dcbb0
310mkt <satoumakoto0609@gmail.com>
2025-04-23 00:09:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ async def main():
     async with TikTokApi() as api:
         await api.create_sessions(
             num_sessions=1,
-            headless=False,
+            headless=True,
             browser='webkit'
         )
         
