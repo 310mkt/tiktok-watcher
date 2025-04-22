62d39f6a23917b8a1ca46db44ba3aa377798cf7b
310mkt <satoumakoto0609@gmail.com>
2025-04-23 00:29:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -66,9 +66,9 @@ async def main():
             browser_args=[
                 "--headless",
                 "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-            ],
-　　　　　)
-        
+            ]
+        )
+
         for name, data in tiktok_users.items():
             sec_uid = data["sec_uid"]
             secret_key = data["secret"]
